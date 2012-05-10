f1db5963f254f5ee9dcbf4cb5e14b58d7d8f355b
fbier <fbier@live.de>
2012-05-10 15:40:42 +0000

    adding ForAllExtended.java

--- JMLExamples/trunk/src/ForAll.java
+++ JMLExamples/trunk/src/ForAll.java
@@ -6,7 +6,11 @@ public class ForAll {
 	}
 	
 	public static void main (String[] args) {
+		System.out.println("Precondition holds!");
 		forAll(new int[] {0, 1, 2, 3});
+		
+		System.out.println("Precondition fails!");
+		forAll(new int[] {0, 1, 1, 3});
 	}
 	
 }
