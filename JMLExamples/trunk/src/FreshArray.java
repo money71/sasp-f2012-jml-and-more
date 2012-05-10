971783f024c238734b87b7f5db411b72e07c3074
pellekrogholt <pellekrogholt@gmail.com>
2012-05-10 20:27:02 +0000

    r66 continued

--- JMLExamples/trunk/src/FreshArray.java
+++ JMLExamples/trunk/src/FreshArray.java
@@ -2,18 +2,22 @@
  *  jml2 refman:
  *  \fresh(x,y) asserts that x and y are not null and that the objects bound to
  *   these identifiers were not allocated in the pre-state. The arguments to \fresh...
+ *   
+ *   \fresh usage think resize() of data structures e.g. using array for storage
+ *   hash map, bag/stack etc.
  */
 public class FreshArray {
-    
-	private static /*@ spec_public @*/ int[] a;
+
+	private static /*@ spec_public @*/ int[] a = new int[1];
 	
     /*@ assignable a; 
     @ ensures \fresh(a);
     @*/
     public static void foo () {
-    	a = new int[2];
+    	a = new int[2*a.length+1];
     	a[0] = 1;
-    	a[1] = 1;
+    	a[1] = 2;
+    	a[2] = 3;
     }
 
     public static void main (String[] args){
