13f467047513ccd247095467916f86907af4a26a
pellekrogholt@gmail.com <pellekrogholt@gmail.com@baa6543e-0093-2654-7586-5298eef6a5a1>
2012-05-11 14:55:02 +0000

    quick api try out

--- OpenJMLExamples/trunk/src/TestOpenJMLAPI.java
+++ OpenJMLExamples/trunk/src/TestOpenJMLAPI.java
@@ -1,8 +1,13 @@
+import java.io.File;
 import java.io.IOException;
 import org.jmlspecs.openjml.API;
+import org.jmlspecs.openjml.JmlTree.JmlCompilationUnit;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.sun.tools.javac.tree.JCTree;
 
 
 public class TestOpenJMLAPI {
@@ -11,11 +16,9 @@ public class TestOpenJMLAPI {
 	private static API api;
 
 	/**
-	 * Setup API
-	 * 
-	 * Will be perform before each test case 
+	 * Setup API 
 	 */	
-	@Before 
+	@BeforeClass 
 	public void setupSearchEngine() throws IOException {
 
 		api = new API();		
@@ -27,36 +30,26 @@ public class TestOpenJMLAPI {
 	public void testCallOpenJMLAPI() throws IOException 
 	{
 	
-//		public void indexFile(String fileName) throws IOException {
-//
-//			// We assume file is in UTF-8 text format.
-//			BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "UTF-8"));
-//
-//			int count = 0;
-//
-//			while (true) {
-//				String s = r.readLine();
-//				if (s == null) break; // no more lines
-//				lines.add(s);
-//
-//				//add word to map and line
-//				addIndex(s, count);
-//				count++;
-//
-//			}
-//
-//		}
-//		
-//		
-//		String dir = System.getProperty("user.dir") + System.getProperty("file.separator");		   
-//        engine = new FileSE();
-//        engine.indexFile(dir+"src/pellekrogholt/algorithm_assignment_5/shells.txt");
-//		
-//		File file = 
-//		api.parseSingleFile(file);
-//		
-////		String result = "<p><b>Query string</b>: the  <b>Lines</b>: 0, 1, 2, </p>";
-////		Assert.assertEquals(engine.search("the"), result);		
+		String dir = System.getProperty("user.dir") + System.getProperty("file.separator");		   
+		File file = new File(dir+"src/ForAll.txt");
+		JmlCompilationUnit f =  api.parseSingleFile(file);
+		//Object tree = f.clone();
+		JCTree tree = f.getTree();
+		// do we have a *real* ast here ?
+		// if so figure out:
+		// 1) how to traverse the tree
+		// 2) print it out in a more tree structure fashion		
+		
+		//api.prettyPrint(ast, likeSource)
+		
+		System.out.println("f:");
+		System.out.println(f);
+
+		System.out.println("tree:");
+		System.out.println(tree);
+		
+		Assert.assertNull(f);
+		Assert.assertNull(tree);
 
 		}	
 	
