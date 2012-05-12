fe68486617268b141c186d2da3c2cfe53ebe1875
pellekrogholt@gmail.com <pellekrogholt@gmail.com@baa6543e-0093-2654-7586-5298eef6a5a1>
2012-05-12 15:19:49 +0000

    r100 continued

--- OpenJMLExamples/trunk/src/dk/itu/mathmodels/quantifiers/ForAll.java
+++ OpenJMLExamples/trunk/src/dk/itu/mathmodels/quantifiers/ForAll.java
@@ -28,7 +28,6 @@ public class ForAll {
 		return i < 10;
 	}
 
-	
 	//@ requires (\forall int i; 0 <= i && i < 10; i == intPredicate(i));
 	public static boolean forall() {
 		boolean auxileryResult = true;
@@ -74,5 +73,49 @@ public class ForAll {
 			// do nothing;
 		}
 	}
+	
+	
+    
+	
+	//@ requires (\forall int i; 0 <= i && i < array.length; array[i-1] == i);
+    public static boolean forallArray (int[] array) { 
+    	int i = 0 + 1; // hmm hack
+    	// here he can move in the whole guard
+    	while(0 <= i && i < array.length){
+    		System.out.println(i);
+			boolean result = (0 <= i && i < array.length && array[i-1] == i);
+			// can we have more predicates than [array[i-1] == i] ?
+			assert(result);
+    		i++;
+    	}
+    	return true;
+    }
+	
+    // - not valid using: ++ http://code.google.com/p/sasp-f2012-jml-and-more/issues/detail?id=8
+    // @ requires (\forall int i, j; 0 <= i && i < array.length && j == i++; array[i] == j);
+    //@ requires (\forall int i, j; 0 <= i && i < array.length && j == i; array[i] == j);
+    public static boolean forallArray2 (int[] array) {
+    	// how do we know i start at 1 ?
+    	int i = 0, j = 0;
+    	// here he can move in the whole guard
+    	while(0 <= i && i < array.length && j == i++){
+    		System.out.println(i);
+			boolean result = (array[i] == j);
+			assert(result);
+    		i++;
+    		j++;
+    	}
+    	return true;
+    }
+
+    
+	
+	
+	
+	
+	
+	
+	
+	
 		
 }
