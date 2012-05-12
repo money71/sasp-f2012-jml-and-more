d3426c5d61d1518d4fffa96040bb50cda8c6b78c
pellekrogholt <pellekrogholt@gmail.com>
2012-05-12 15:18:24 +0000

    various adjusts and ++ not allowed in jml quanti #8

--- JMLExamples/trunk/src/ForAllMultipleVariables.java
+++ JMLExamples/trunk/src/ForAllMultipleVariables.java
@@ -1,6 +1,7 @@
 
 public class ForAllMultipleVariables {
-	
+
+    // - not valid using: ++ http://code.google.com/p/sasp-f2012-jml-and-more/issues/detail?id=8
 	//@ requires (\forall int i, j; 0 <= i && i < array.length && j == i++; array[i] == j);
 	public static void forAllMultipleVars (int[] array) {
 		// Do nothing
