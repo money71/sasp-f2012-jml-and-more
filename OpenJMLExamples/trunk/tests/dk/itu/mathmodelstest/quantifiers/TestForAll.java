a0a27a170c4836e926bdf5439bcc02a119105fd2
pellekrogholt <pellekrogholt@gmail.com>
2012-05-12 15:19:49 +0000

    r100 continued

--- OpenJMLExamples/trunk/tests/dk/itu/mathmodelstest/quantifiers/TestForAll.java
+++ OpenJMLExamples/trunk/tests/dk/itu/mathmodelstest/quantifiers/TestForAll.java
@@ -16,23 +16,30 @@ import dk.itu.mathmodels.quantifiers.ForAll;
 
 public class TestForAll {
 	
-	
-	
+
 	@Test
 	public void testForAll() 
 	{
-	
 		Assert.assertTrue(ForAll.forall());
-
 	}	
 
 	@Test
 	public void testForAllBreak() 
 	{
-	
 		Assert.assertTrue(ForAll.forallBreak());
-
 	}
+
+	
+	@Test
+	public void testForAllArray() 
+	{
+		int[] array = {0,1,2,3,4,5,6,7,8,9};
+		Assert.assertTrue(ForAll.forallArray(array));
+		
+		Assert.assertTrue(ForAll.forallArray2(array));
+		
+	}
+
 	
 	
 	
