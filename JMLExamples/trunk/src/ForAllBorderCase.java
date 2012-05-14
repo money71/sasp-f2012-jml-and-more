f227081d14550a693fc147c5e8c79441796b987f
fbier@live.de <fbier@live.de@baa6543e-0093-2654-7586-5298eef6a5a1>
2012-05-14 10:17:58 +0000

    added borderline case example for forall expression

---
+++ JMLExamples/trunk/src/ForAllBorderCase.java
@@ -0,0 +1,49 @@
+
+class Foo {
+	public int value;
+	
+	public Foo(int value){
+		this.value = value;
+	}
+	
+	public boolean p(){
+		System.out.println("Evaluation in Foo was executed!");
+		return value > 10;
+	}
+}
+
+class Bar {
+	private int value;
+	
+	public Bar(int value){
+		this.value = value;
+	}
+	
+	public int getValue(){
+		return this.value;
+	}
+	
+	public boolean p(){
+		System.out.println("Evaluation in Bar was executed!");
+		return this.getValue() > 10;
+	}
+}
+
+public class ForAllBorderCase {
+	
+	//@ requires (\forall Foo f; 10 < f.value && f.value < 20; f.p()); 
+	public static void forAllBorderCaseFoo(){
+		// Do nothing
+	}
+	
+	//@ requires (\forall Bar b; 10 < b.getValue() && b.getValue() < 20; b.p());
+	public static void forAllBorderCaseBar(){
+		// Do nothing
+	}
+	
+	public static void main(String[] args) {
+		forAllBorderCaseFoo();
+		forAllBorderCaseBar();
+	}
+
+}
