293deeefec5937ce724311c40002990c2a1c82c6
pellekrogholt@gmail.com <pellekrogholt@gmail.com@baa6543e-0093-2654-7586-5298eef6a5a1>
2012-05-16 10:18:56 +0000

    finalazed the example with an iterable sequence

--- OpenJMLExamples/trunk/src/dk/itu/openjml/rangeextended/collection.java
+++ OpenJMLExamples/trunk/src/dk/itu/openjml/rangeextended/collection.java
@@ -1,10 +1,23 @@
 package dk.itu.openjml.rangeextended;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.util.List; 
 import java.util.Arrays;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Date;
 
+//import dk.itu.openjml.rangeextended.Range;
+
+/**
+ * 
+ * Based up xxx
+ * in this article he name the class with a lower case (normal approach capitalize)
+ * we guess its related to a kind of pattern since its not a real class/interface 
+ * more a wrapper class for simplified instantialization...
+ * 
+ */
 public class collection {
 
 	public static <T> List<T> List(T...elems){
@@ -19,5 +32,24 @@ public class collection {
 	public static <T> Set<T> Set(T...elems){
 		return new HashSet<T>( List( elems ) );
 	}
+
+	// Don't use <S extends Comparable<T>>
+	public static <S extends Comparable<S>> Range<S> Range( S from, S to ){
+		return new Range( from, to );
+	}
+	
+	// FIXME: look into this one not working yet
+	// enable test if going to look into it
+	public static Date Date( String date ){
+		try {
+			return DateFormat.getDateInstance().parse( date );
+		}
+		catch (ParseException e) {
+			throw new RuntimeException( e );
+		}
+	}
+	
+	
+	
 	
 }
