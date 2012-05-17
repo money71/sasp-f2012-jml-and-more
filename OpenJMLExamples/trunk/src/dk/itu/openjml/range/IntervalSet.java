e65bde8afc3a3a5fd3db0901ec2ce61742568b3e
fbier <fbier@live.de>
2012-05-17 15:03:05 +0000

    - implemented a working interval set for union, intersection and intersected union + tests \n- united intersection is *not* working properly

--- OpenJMLExamples/trunk/src/dk/itu/openjml/range/IntervalSet.java
+++ OpenJMLExamples/trunk/src/dk/itu/openjml/range/IntervalSet.java
@@ -6,18 +6,19 @@ import java.util.Iterator;
  * A set of intervals over integers. The Interval is a
  * subtype of IntervalSet and can be regarded as a singleton
  * of IntervalSet.
+ * 
+ * @note Interval is *left-inclusive* and *right-exclusive*!
  */
-public abstract class IntervalSet implements Iterator{
-	
-	class NotIncluded extends Exception{
-		public NotIncluded(int i){
-			super("Number not included in interval: " + i);
-		}
-	}
+public abstract class IntervalSet implements Iterator<Integer>{
 	
 	IntervalSet left;
 	IntervalSet right;
-	int current;
+	
+	private boolean initialized;
+	
+	int low;
+	int high;
+	protected int current;
 	
 	/**
 	 * Performs union on two IntervalSets
@@ -58,25 +59,44 @@ public abstract class IntervalSet implements Iterator{
 		left = l;
 		right = r;
 		current = Integer.MIN_VALUE;
+		initialized = false;
 	}
 	
 	@Override
 	public boolean hasNext() {
-		// Check if we reached maximum representable value 
-		if(current == Integer.MAX_VALUE){
-			return false;
+		// Get the next valid range
+		if(!initialized || current == high){
+			initialized = true;
+			getNextRange();
 		}
-		return isInside(current);
+		return low <= current && current < high;
 	}
 
 	@Override
-	public Object next(){
-		return getNext(this.current);
+	public Integer next(){
+		// Check sets up all the ranges, just in case
+		if(hasNext()){
+			int r = current;
+			current++;
+			return r;
+		}
+		return current;
 	}
 
 	@Override
 	public void remove(){
 		// Does nothing
+	}
+	
+	/**
+	 * Find the next valid range for this IntervalSet
+	 */
+	protected void getNextRange(){
+		low = getNextLow(current);
+		high = getNextHigh(current);// + 1; // FIXME: This is a hack to get inclusive high boundary
+		
+		// Set current to the new low
+		current = low;
 	}
 	
 	/**
@@ -86,7 +106,19 @@ public abstract class IntervalSet implements Iterator{
 	 */
 	abstract protected boolean isInside(int current);
 	
-	abstract protected int getNext(int current);
+	/**
+	 * Returns the next valid low after current
+	 * @param current The current number active.
+	 * @return The new low. If there is no valid new low, return current again.
+	 */
+	abstract protected int getNextLow(int current);
+	
+	/**
+	 * Returns the next valid high for current 
+	 * @param current
+	 * @return
+	 */
+	abstract protected int getNextHigh(int current);
 }
 
 /**
@@ -103,13 +135,47 @@ class UnionIntervalSet extends IntervalSet {
 		return left.isInside(current) || right.isInside(current);
 	}
 	
-	protected int getNext(int current){
+	/**
+	 * Union can answer two different lows!
+	 */
+	protected int getNextLow(int current){
+		int l = left.getNextLow(current);
+		int r = right.getNextLow(current);
 		
-		int l = left.getNext(current);
-		int r = right.getNext(current);
+		// If both are higher than current
+		if(current < l && current < r){
+			// Return the smaller low
+			return l < r ? l : r;
 		
-		// Return the number with the lowest absolute distance
-		return Math.abs(l - current) < Math.abs (r - current)? l : r;
+		// Else return the one that is higher than current
+		} else if(current < l){
+			return l;
+		} else if(current < r){
+			return r;
+		}
+		
+		// If none of this holds, return current again
+		return current;
+	}
+
+	protected int getNextHigh(int current) {
+		int l = left.getNextHigh(current);
+		int r = right.getNextHigh(current);
+		
+		// If both are higher than current
+		if(current < l && current < r){
+			// Return the smaller high
+			return l < r ? l : r;
+		
+		// Else return the one that is higher than current
+		} else if(current < l){
+			return l;
+		} else if(current < r){
+			return r;
+		}
+		
+		// If none of this holds, return current again
+		return current;
 	}
 }
 
@@ -123,30 +189,46 @@ class IntersectionIntervalSet extends IntervalSet {
 	}
 
 	//@ assignable left.current, right.current;
-	@Override
 	public boolean isInside(int current){
 		this.current = current;
 		return left.isInside(current) && right.isInside(current);
 	}
 
-	@Override
-	protected int getNext(int current) {
-		int l = left.getNext(current);
-		int r = right.getNext(current);
+	protected int getNextLow(int current) {
+		int l = left.getNextLow(current);
+		int r = right.getNextLow(current);
 		
-		// Return the number with the lowest absolute distance
-		return Math.abs(l - current) < Math.abs (r - current)? l : r;
+		// When current is lower than any of these, return
+		// the higher low.
+		if(current < l || current < r){
+			return l < r ? r : l;
+		}
+		
+		// Else just return current
+		return current;
+	}
+
+	protected int getNextHigh(int current) {
+		int l = left.getNextHigh(current);
+		int r = right.getNextHigh(current);
+		
+		// When current is lower than any of these, return
+		// the lower high.
+		if(current < l || current < r){
+			return l < r ? l : r;
+		}
+		
+		// Else just return current
+		return current;
 	}
 }
 
 /**
  * Represents a singleton of IntervalSet
+ * It is left-inclusive and right-exclusive!
  */
 class Interval extends IntervalSet {
-	
-	int low;
-	int high;
-	
+		
 	/**
 	 * Creates an actual inclusive interval
 	 * @param low Lower boundary
@@ -159,21 +241,22 @@ class Interval extends IntervalSet {
 		this.current = this.low;
 	}
 	
-	@Override
-	public  boolean hasNext() {
-		return isInside(current + 1);
+	public boolean hasNext() {
+		return isInside(current);
 	}
 	
 	//@ ensures \result == low <= current && current <= high;
 	protected /*@ pure @*/ boolean isInside(int current){
 		return low <= current && current <= high;
 	}
-
-	@Override
-	protected int getNext(int current) {
-		if(current < low) return low;
-		if(current > high) return high;
-		this.current = current + 1;
-		return this.current;
+	
+	//@ ensures \result == this.low
+	protected int getNextLow(int current) {
+		return low;
+	}
+	
+	//@ ensures \result == this.high
+	protected int getNextHigh(int current) {
+		return high;
 	}
 }
--- OpenJMLExamples/trunk/src/dk/itu/openjml/test/Test_IntervalSet.java
+++ OpenJMLExamples/trunk/src/dk/itu/openjml/test/Test_IntervalSet.java
@@ -1,6 +1,7 @@
 package dk.itu.openjml.test;
 
 import static org.junit.Assert.*;
+import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,15 +15,68 @@ public class Test_IntervalSet {
 	}
 
 	@Test
-	public void test() {
-		IntervalSet p = IntervalSet.union(IntervalSet.interval(0, 9), IntervalSet.interval(11, 20));
-		
-		String r = "";
-		while(p.hasNext()){
-			r += p.next() + ", ";
+	public void testUnionGap() {
+		IntervalSet u = IntervalSet.union(IntervalSet.interval(0, 10), IntervalSet.interval(11, 20));
+		try{
+			String su = "Union: ";
+			while(u.hasNext()){
+				int n = u.next();
+				su += n + "; ";
+				assert (0 <= n && n <= 9) || (11 <= n && n <= 19);
+			}
+			System.out.println(su);
+		} catch (Exception e){
+			Assert.fail();
 		}
-		
-		System.out.println(r);
 	}
-
+	
+	@Test
+	public void testIntersection() {
+		IntervalSet i = IntervalSet.intersect(IntervalSet.interval(0, 11), IntervalSet.interval(5, 15));
+		try{
+		String si = "Intersection: ";
+		while(i.hasNext()){
+			int n = i.next();
+			si += n + "; ";
+			assert (0 <= n && n <= 10) && (5 <= n && n <= 14);
+		}
+		System.out.println(si);
+		} catch (Exception e){
+			Assert.fail();
+		}
+	}
+	
+	@Test
+	public void testIntersectedUnion() {
+		IntervalSet u = IntervalSet.union(IntervalSet.interval(0, 11), IntervalSet.interval(20, 31));
+		IntervalSet iu = IntervalSet.intersect(u, IntervalSet.interval(5, 26));
+		try{
+		String siu = "Intersected union: ";
+		while(iu.hasNext()){
+			int n = iu.next();
+			siu += n + "; ";
+			assert (0 <= n && n <= 10 || 20 <= n && n <= 30) && (5 <= n && n <= 25);
+		}
+		System.out.println(siu);
+		} catch (Exception e){
+			Assert.fail();
+		}
+	}
+	
+	@Test
+	public void testUnitedIntersection() {
+		IntervalSet i = IntervalSet.intersect(IntervalSet.interval(0, 101), IntervalSet.interval(50, 151));
+		IntervalSet ui = IntervalSet.intersect(i, IntervalSet.interval(5, 26));
+		try{
+		String sui = "United intersection: ";
+		while(ui.hasNext()){
+			int n = ui.next();
+			sui += n + "; ";
+			assert (0 <= n && n <= 100) && (50 <= n && n <= 150) || (5 <= n && n <= 25);
+		}
+		System.out.println(sui);
+		} catch (Exception e){
+			Assert.fail();
+		}
+	}
 }
