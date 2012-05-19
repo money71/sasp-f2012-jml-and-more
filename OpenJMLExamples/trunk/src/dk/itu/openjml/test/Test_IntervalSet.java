0a0e9af63c5511fe341fb01d771895017bd45363
pellekrogholt <pellekrogholt@gmail.com>
2012-05-19 13:05:46 +0000

    small adjusts preparing for #20, #18

--- OpenJMLExamples/trunk/src/dk/itu/openjml/test/Test_IntervalSet.java
+++ OpenJMLExamples/trunk/src/dk/itu/openjml/test/Test_IntervalSet.java
@@ -1,6 +1,9 @@
 package dk.itu.openjml.test;
 
 import static org.junit.Assert.*;
+
+import java.util.Iterator;
+
 import junit.framework.Assert;
 
 import org.junit.Before;
@@ -13,15 +16,48 @@ public class Test_IntervalSet {
 	@Before
 	public void setUp() throws Exception {
 	}
+	
+	
+	@Test
+	public void testIntervalSetBasic() {
+				
+		IntervalSet i = IntervalSet.interval(0, 10);		
+		assertNotNull(i);
+		
+	}
+
+	@Test
+	public void testIntervalSetBasicIterator() {
+				
+		IntervalSet i = IntervalSet.interval(0, 10);
+
+		int count = 0;
+		for(int n: i){
+			assertNotNull(n);
+			count++;
+		}
+		
+		assertEquals(11, count);
+		
+		/* FIXME: before doing for each I tried a quick Iterator example but it raised infinite loop
+			Iterator<Integer> ite = i.iterator();
+			while(ite.hasNext()){
+				System.out.println(ite.toString());
+			}
+			- whats going on ?
+		 */		
+	}
+	
 
 	@Test
 	public void testUnionGap() {
 		IntervalSet u = IntervalSet.union(IntervalSet.interval(0, 10), IntervalSet.interval(11, 20));
+		
 		try{
 			String su = "Union: ";
 			for(int n: u){
 				su += n + "; ";
-				assert 0 <= n && n <= 9 || 11 <= n && n <= 19;
+				assertTrue(0 <= n && n <= 9 || 11 <= n && n <= 19);
 			}
 			System.out.println(su);
 		} catch (Exception e){
@@ -36,7 +72,7 @@ public class Test_IntervalSet {
 			String si = "Intersection: ";
 			for(int n: i){
 				si += n + "; ";
-				assert 0 <= n && n <= 10 && 5 <= n && n <= 14;
+				assertTrue(0 <= n && n <= 10 && 5 <= n && n <= 14);
 			}
 		System.out.println(si);
 		} catch (Exception e){
@@ -52,7 +88,7 @@ public class Test_IntervalSet {
 			String siu = "Intersected union: ";
 			for(int n: iu){
 				siu += n + "; ";
-				assert 0 <= n && n <= 10 || 20 <= n && n <= 30 && 5 <= n && n <= 25;
+				assertTrue(0 <= n && n <= 10 || 20 <= n && n <= 30 && 5 <= n && n <= 25);
 			}	
 			System.out.println(siu);
 		} catch (Exception e){
@@ -68,7 +104,7 @@ public class Test_IntervalSet {
 			String sui = "United intersection: ";
 			for(int n: ui){
 				sui += n + "; ";
-				assert 0 <= n && n <= 100 && 50 <= n && n <= 150 || 40 <= n && n <= 60;
+				assertTrue(0 <= n && n <= 100 && 50 <= n && n <= 150 || 40 <= n && n <= 60);
 			}
 			System.out.println(sui);
 		} catch (Exception e){
@@ -83,7 +119,7 @@ public class Test_IntervalSet {
 			String sni = "Union without 50: ";
 			for(int n: ni){
 				sni += n + "; ";
-				assert 0 <= n && n <= 100 && n != 50;
+				assertTrue(0 <= n && n <= 100 && n != 50);
 			}
 			System.out.println(sni);
 		} catch (Exception e){
