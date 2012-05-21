4caff1212fc0efc280f3f34c876063be84f3364a
pellekrogholt@gmail.com <pellekrogholt@gmail.com@baa6543e-0093-2654-7586-5298eef6a5a1>
2012-05-21 08:59:35 +0000

    its tag time

---
+++ OpenJMLExtended/tags/20120521_1/tests/dk/itu/openjml/quantifiers/TestAll.java
@@ -0,0 +1,15 @@
+package dk.itu.openjml.quantifiers;
+
+import org.junit.runner.RunWith;  
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( {  
+	Test_ForAll.class,
+	Test_IntervalSet.class,
+	Test_QRange.class
+} )
+public class TestAll {
+
+}
+
