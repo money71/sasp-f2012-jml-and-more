5cf2b3674a3e5bfa2e46700e38b646bdbabd3107
pellekrogholt@gmail.com <pellekrogholt@gmail.com@baa6543e-0093-2654-7586-5298eef6a5a1>
2012-06-13 07:57:15 +0000

    oh it was pretty simple just specific the class the rac need to know about...

---
+++ OpenJMLExtended/branches/ad_hoc_improvements_june_2012_ticket_31/tests/dk/itu/openjml/quantifiers/Utils.java
@@ -0,0 +1,11 @@
+package dk.itu.openjml.quantifiers;
+
+public class Utils {
+
+	
+	// Reguired for \forall expressions referencing 'array' #31 
+	public static int[] array = {1,2,3,4};
+	public final static int[] arrayFinal = {1,2,3,4};
+	
+	
+}
